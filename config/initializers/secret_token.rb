7597d6375e1bca8c0ee4f7d1a807dc607887487b
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-06 20:11:51 -0400

    deploy configs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoftwearProduction::Application.config.secret_key_base = '4abc745146a940c699919eebe80e0ea120270d2fb7a53c6517b0de1fd3858125b0ec01329af0de5ff2862273319c3471713e1e8e34d27a19b0475ceb3179064c'
