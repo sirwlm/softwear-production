b59b8d7c6263851eb836a6eb1f468bfb3e19d4cf
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-06-02 00:59:41 -0400

    stupid mistake in whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, {:error => '/home/ubuntu/RailsApps/production.softwearcrm.com/shared/log/cron.error.log',
               :standard => '/home/ubuntu/RailsApps/production.softwearcrm.com/shared/log/cron.log'}
 
-ob_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment /home/ubuntu/.rvm/wrappers/ruby-2.1.1/bundle exec rake :task --silent :output"
+job_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment /home/ubuntu/.rvm/wrappers/ruby-2.1.1/bundle exec rake :task --silent :output"
 
 every 3.minutes do
   rake "screens:dry"
