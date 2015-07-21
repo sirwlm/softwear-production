3237af937da071a02263fd1d15ed30d3e0773fe7
Ben Cooper <coobenja@umich.edu>
2015-07-21 16:52:06 -0400

    Added public activity text fields to ImprintableTrains

--- app/models/imprintable_train.rb
+++ app/models/imprintable_train.rb
@@ -1,5 +1,8 @@
 class ImprintableTrain < ActiveRecord::Base
   include Train
+  include PublicActivity::Model
+
+  tracked only: [:transition]
 
   attr_accessor :solution
 
@@ -8,7 +11,7 @@ class ImprintableTrain < ActiveRecord::Base
   train_type :pre_production
   train initial: :ready_to_order do
 
-    success_event :some_pieces_ordered do
+    success_event :some_pieces_ordered, public_activity: { supplier: :text_field, location: :text_field, estimated_arrival_date: :text_field } do
       transition [:ready_to_order, :partially_ordered] => :partially_ordered
     end
 
@@ -16,15 +19,15 @@ class ImprintableTrain < ActiveRecord::Base
       transition [:ready_to_order, :partially_ordered] => :ordered
     end
 
-    success_event :some_pieces_arrive do
+    success_event :some_pieces_arrive, public_activity: { input: :text_field, location: :text_field } do
       transition [:ordered, :partially_inventoried] => :partially_inventoried
     end
 
-    success_event :all_pieces_arrived do
+    success_event :all_pieces_arrived, public_activity: { input: :text_field, location: :text_field } do
       transition [:ordered, :partially_inventoried] => :inventoried
     end
 
-    success_event :pieces_on_cart do
+    success_event :pieces_on_cart, public_activity: { input: :text_field, location: :text_field } do
       transition :inventoried => :staged
     end
 
