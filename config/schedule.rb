e9d14524308d1ad17d1af6ba1bc3f00ca36fbe6c
Nigel Baillie <metreckk@gmail.com>
2015-10-08 14:11:55 -0400

    make whenever work possibly

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, {:error => '/home/ubuntu/RailsApps/production.softwearcrm.com/shared/log/cron.error.log',
               :standard => '/home/ubuntu/RailsApps/production.softwearcrm.com/shared/log/cron.log'}
 
-job_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment /home/ubuntu/.rvm/wrappers/ruby-2.1.1/bundle exec rake :task --silent :output"
+job_type :rake, "{ cd #{@current_path || ':path'} > /dev/null; } && RAILS_ENV=:environment rvm 2.1.1 do bundle exec rake :task --silent :output"
 
 every 1.minute do
   rake "screens:dry"
