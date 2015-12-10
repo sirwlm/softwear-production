53cf3861f3f00d6eaeb4e76ecd885e6d0f274ce6
Nigel Baillie <metreckk@gmail.com>
2015-12-10 16:15:36 -0500

    parse time before adding it to range for calendar

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -36,7 +36,7 @@ class MachinesController < InheritedResources::Base
     end
 
     @calendar_events = Sunspot.search(*Schedulable.schedulable_classes) do
-      with :scheduled_at, time_start..time_end
+      with :scheduled_at, Time.parse(time_start)..Time.parse(time_end)
       with :machine_id, machine_id
       paginate page: 1, per_page: 5000
     end
