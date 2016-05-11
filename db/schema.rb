1db318d35189486b35bcbb8a0a4e5c27a8f922fd
Stefan Gale <stefan@wolverinetshirtcompany.com>
2016-05-11 14:24:56 -0400

    Changed the schema

--- db/schema.rb
+++ db/schema.rb
@@ -112,13 +112,13 @@ ActiveRecord::Schema.define(version: 20160510192727) do
     t.datetime "scheduled_at"
     t.decimal  "estimated_time",                 precision: 10, scale: 2
     t.datetime "estimated_end_at"
-    t.datetime "created_at",                                                              null: false
-    t.datetime "updated_at",                                                              null: false
+    t.datetime "created_at",                                              null: false
+    t.datetime "updated_at",                                              null: false
     t.integer  "order_id",           limit: 4
     t.datetime "completed_at"
     t.string   "previous_state",     limit: 191
     t.string   "inventory_location", limit: 191
-    t.boolean  "printed",                                                 default: false
+    t.boolean  "printed"
   end
 
   create_table "fba_label_trains", force: :cascade do |t|
@@ -324,6 +324,10 @@ ActiveRecord::Schema.define(version: 20160510192727) do
     t.string   "previous_state",       limit: 191
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name", limit: 191
+  end
+
   create_table "screen_requests", force: :cascade do |t|
     t.integer  "screen_train_id", limit: 4
     t.string   "frame_type",      limit: 191
@@ -449,6 +453,41 @@ ActiveRecord::Schema.define(version: 20160510192727) do
   end
 
   add_index "train_autocompletes", ["field"], name: "index_train_autocompletes_on_field", using: :btree
+
+  create_table "user_roles", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
+    t.integer  "role_id",    limit: 4
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 191
+    t.string   "encrypted_password",     limit: 191
+    t.string   "reset_password_token",   limit: 191
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 191
+    t.string   "last_sign_in_ip",        limit: 191
+    t.string   "confirmation_token",     limit: 191
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 191
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.string   "first_name",             limit: 191
+    t.string   "last_name",              limit: 191
+    t.string   "authentication_token",   limit: 191
+    t.string   "default_view",           limit: 191
+  end
+
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "warning_emails", force: :cascade do |t|
     t.string  "model",     limit: 191
