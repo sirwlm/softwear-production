4affbe1f93b566882e7c0f09d938c9ae3b201235
Stefan Gale <stefan@wolverinetshirtcompany.com>
2016-11-30 14:37:29 -0500

    Fixed the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'public_activity/testing'
-require 'softwear'
+require 'softwear/lib'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
