03ef42909eda40a8329d62078d41935ac42fa683
Nigel Baillie <metreckk@gmail.com>
2016-12-05 14:35:53 -0500

    include controllerauthentication in userscontroller (it wasn't already there somehow)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 require 'softwear/auth/controller'
 
 class UsersController < Softwear::Auth::Controller
+  include Softwear::Library::ControllerAuthentication
+
   def sign_out
     session[:user_token] = nil
     redirect_to destroy_user_session_path
